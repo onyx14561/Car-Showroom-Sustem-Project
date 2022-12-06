aa8a6cccb3faa2729116246c310042a8a930dc01
Rutij Navelkar <MY_NAME@example.com>
2022-12-06 19:58:38 +0530

    Modified

--- dhome.php
+++ dhome.php
@@ -10,7 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Dealer Page</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">-->
     <link rel="stylesheet" href="css/stl.css">
 
 </head>
